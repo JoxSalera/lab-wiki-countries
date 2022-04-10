17ad1e5844e2947ce6aa8ec68349bbccf6e99847
JoxSalera <97227244+JoxSalera@users.noreply.github.com>
2022-04-10 23:53:50 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState();
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const { data } = await axios.get(
+        'https://ih-countries-api.herokuapp.com/countries'
+      );
+      setCountries(data);
+    };
+
+    fetchCountries();
+  }, [countries]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<CountriesList />} />
+          <Route
+            path="countries/:alpha3Code"
+            element={<CountryDetails countries={countries} />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/CountriesList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import countries from '../countries.json';
+
+const CountriesList = () => {
+  const countriesData = countries.map((country, i) => {
+    return (
+      <Link to={`/countries/${country.alpha3Code}`}>
+        {' '}
+        {country.name.common}{' '}
+      </Link>
+    );
+  });
+
+  return <div className="countries-list">{countriesData}</div>;
+};
+
+export default CountriesList;
